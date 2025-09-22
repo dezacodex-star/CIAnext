b548bc44bae35e2a7f8bd00233f021795a872910
mohan-2427 <lovelymohan2427@gmail.com>
2025-09-22 11:56:23 +0530

    resolving error 01

--- proj/settings.py
+++ proj/settings.py
@@ -132,8 +132,8 @@ DATABASES = {
         'PORT': '6543',
     }
 }
-'''
 
+'''
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -163,15 +163,19 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+'''
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'app/static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+'''
 # Media files (Uploaded images)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
